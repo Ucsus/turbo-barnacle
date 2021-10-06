cbdd37ca7c6f3cffadacbcf8ea7dea67c27c037a
Ucsus <ucsusman@gmail.com>
2021-10-06 14:55:44 +0300

    homework

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
 
 const personaMovieDB = {
     count: numberOfFilms,
@@ -10,28 +20,36 @@ const personaMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-          b = prompt("На сколько его оцените?", "");
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько его оцените?", "");
 
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-        personaMovieDB.movies[a] = b;
-        console.log("Done");
-    } else {
-        console.log("Error");
-        i--;
+        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personaMovieDB.movies[a] = b;
+            console.log("Done");
+        } else {
+            console.log("Error");
+            i--; // вернуться на одну итерацию назад
+        }
+
     }
-
 }
 
-if (personaMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30) {
-    console.log("Круто");
-} else if (personaMovieDB.count >= 30) {
-    console.log("Киноман");
-} else {
-    console.log("Ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personaMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30) {
+        console.log("Круто");
+    } else if (personaMovieDB.count >= 30) {
+        console.log("Ну охуеть теперь!");
+    } else {
+        console.log("Ошибка");
+    }
+    
+    console.log(personaMovieDB);
 }
 
-console.log(personaMovieDB); 
+detectPersonalLevel();
